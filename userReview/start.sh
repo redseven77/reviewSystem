a3ada9392a496be1d25d4c681845989d48d532fa
shashi.mishra <shashi.mishra@vphrase.com>
2020-06-07 18:57:28 +0000

    fix: start.sh

--- userReview/start.sh
+++ userReview/start.sh
@@ -4,5 +4,5 @@ echo "----------------UPLOADING DATA----------------------"
 python3 manage.py load_data --name foods.txt
 echo $?
 echo "----------------STARTING SERVER----------------------"
-nohup python3 manage.py runserver 0.0.0.0:8000 1>&2 nohup.out
+nohup `python3 manage.py runserver 0.0.0.0:8000` 1>&2 nohup.out
 echo $?
